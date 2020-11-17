beefb214b8c8d9078b712ed92196da3a09b8bf30
hyeonse <dlgustp1487@naver.com>
2020-11-17 21:07:22 +0900

    환경세팅 완료

---
+++ src/main/java/com/lhs/blog/SpringBootStudyApplication.java
@@ -0,0 +1,13 @@
+package com.lhs.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStudyApplication.class, args);
+	}
+
+}
