5e2acc16e90f0562004b488692047fccd375ac91
hyeonse <dlgustp1487@naver.com>
2020-11-19 21:28:37 +0900

    lombok세팅 및 사용해보기

--- src/main/java/com/lhs/blog/test/HttpControllerTest.java
+++ src/main/java/com/lhs/blog/test/HttpControllerTest.java
@@ -15,26 +15,34 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HttpControllerTest {
 
+	private static final String TAG = "HttpControllerTest: ";
+
+	@GetMapping("/http/lombok")
+	public String lombokTest() {
+		Member m = Member.builder().username("hyeon").password("1234").email("dlgustp1487@naver.com").build();
+		System.out.println(TAG+ "getter : "+ m.getUsername());
+		m.setUsername("cos");
+		System.out.println(TAG+ "setter : "+ m.getUsername());
+		return "lombok test 완료";
+	}
 	//인터넷 브라우저 요청은 무조건 get요청밖에 할 수 없다.
 	//http://localhost:8080/http/get(select)
 	@GetMapping("/http/get")
 	public String getTest(Member m) {
-		return "get요청: " + m.getId() + "," + m.getUsername()+"," + m.getPassword()
-		 + "," + m.getEmail();
+
+		return "get요청: " + m.getId() + "," + m.getUsername()+"," + m.getPassword() + "," + m.getEmail();
 	}
 
 	//http://localhost:8080/http/post(insert)
 	@PostMapping("/http/post")
 	public String postTest(@RequestBody Member m) {//MessageConverter (스프링부트)
-		return "post요청: " + m.getId() + "," + m.getUsername()+"," + m.getPassword()
-		 + "," + m.getEmail();
+		return "post요청: " + m.getId() + "," + m.getUsername()+"," + m.getPassword()	 + "," + m.getEmail();
 	}
 
 	//http://localhost:8080/http/put(update)
 	@PutMapping("/http/put")
 	public String putTest(@RequestBody Member m) {
-		return "put요청"+ m.getId() + "," + m.getUsername()+"," + m.getPassword()
-		 + "," + m.getEmail();
+		return "put요청"+ m.getId() + "," + m.getUsername()+"," + m.getPassword()	 + "," + m.getEmail();
 	}
 
 	//http://localhost:8080/http/delete(delete)
