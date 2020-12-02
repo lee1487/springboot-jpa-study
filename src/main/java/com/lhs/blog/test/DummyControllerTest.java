0abc1999d3bc0f0e5e6052e444a6e5ca146aa349
hyeonse <dlgustp1487@naver.com>
2020-12-02 20:24:20 +0900

    update,delete,exception, json파싱전략

--- src/main/java/com/lhs/blog/test/DummyControllerTest.java
+++ src/main/java/com/lhs/blog/test/DummyControllerTest.java
@@ -1,18 +1,119 @@
 package com.lhs.blog.test;
 
+import java.util.List;
+import java.util.function.Supplier;
+
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lhs.blog.model.RoleType;
 import com.lhs.blog.model.User;
 import com.lhs.blog.repository.UserRepository;
 
+//html파일이 아니라 data를 리턴해주는 controller = RestController
 @RestController
 public class DummyControllerTest {
 
 	@Autowired //의존성 주입(DI)
 	private UserRepository userRepository;
+
+
+	@DeleteMapping("/dummy/user/{id}")
+	public String delete(@PathVariable int id) {
+		try {
+			userRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			// TODO: handle exception
+			return "삭제에 실패하였습니다. 해당 id는 DB에 없습니다.";
+		}
+
+		return "삭제되었습니다. id: " + id;
+	}
+
+	//save함수는  id를 전달하지 않으면 insert를 해주고
+	//save함수는  id를 전달하면 해당 아이디에대한 데이터가 있으면 update를 해주고
+	//save함수는	id를 전달하면 해당 아이디에대한 데이터가 없으면 insert를 해줌
+	//email,password
+	@Transactional	//함수 종료시에 자동 commit됨
+	@PutMapping("/dummy/user/{id}")
+	public User updateUser(@PathVariable int id,@RequestBody User requestUser) {
+		System.out.println("id: " + id);
+		System.out.println("password: " + requestUser.getPassword());
+		System.out.println("email: " + requestUser.getEmail());
+
+		User user = userRepository.findById(id).orElseThrow(() -> {
+			return new IllegalArgumentException("수정에 실패하였습니다.");
+		});
+		user.setPassword(requestUser.getPassword());
+		user.setEmail(requestUser.getEmail());
+
+		//userRepository.save(user);
+
+		//requestUser.setId(id);
+		//requestUser.setUsername("ssar");
+		//userRepository.save(requestUser);
+
+		//더티 체킹
+		return user;
+	}
+
+	//http://localhost:8000/blog/dummy/user
+	@GetMapping("/dummy/users")
+	public List<User> list() {
+		return userRepository.findAll();
+	}
+
+	//한페이지당 2건에 데이터를 리턴받아 볼 예정
+	@GetMapping("/dummy/user")
+	public List<User> pageList(@PageableDefault(size=2, sort="id", direction=Sort.Direction.DESC) Pageable  pageable) {
+		//Page<User> users = userRepository.findAll(pageable);
+		//List<User> users = userRepository.findAll(pageable).getContent();
+
+		Page<User> pagingUser = userRepository.findAll(pageable);
+		List<User> users = pagingUser.getContent();
+		return users;
+	}
+
+	//{id} 주소로 파라미터를 전달 받을 수 있음.
+	//http://localhost:8000/blog/dummy/user/3
+	@GetMapping("/dummy/user/{id}")
+	public User detail(@PathVariable int id) {
+		// user의 4번을 찾으면 내가 데이터베이스에서 못찾아오면 user가 null 이 될거 아냐?
+		// 그럼 return 할 때 null이 return이 되잖아. 그럼 프로그램에 문제가 있지 않겠니?
+		// 그럼 나는 Optional로 너의 user 객체를 감싸서 가져올테니 null인지 아닌지 판단해서 리턴해
+
+		//람다식
+		//User user = userRepository.findById(id).orElseThrow(() -> {
+		//	return new IllegalArgumentException("해당 유저는 없습니다. id: " + id);
+		//});
+
+		User user = userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>() {
+			@Override
+			public IllegalArgumentException get() {
+				// TODO Auto-generated method stub
+				return new IllegalArgumentException("해당 유저는 없습니다. id: " + id);
+			}
+		});
+		//user 객체 = 자바 오브젝트
+		// 변환 (웹브라우저가 이해할 수 있는 데이터) -> json  (Gson 라이브러리)
+		// 스프링부트  = MessageConverter라는 애가 응답시에 자동 작동
+		// 만약에 자바 오브젝트를 리턴하게되면 MessageConverter가 Jackson 라이브러리를 호출해서
+		// user 오브젝트를 json으로 변환해서 브라우저에 던져줍니다.
+		return user;
+	}
 
 	//http://localhost:8000/blog/dummy/join (요청)
 	//http의 body에 username, password, email 데이터를 가지고 (요청)
