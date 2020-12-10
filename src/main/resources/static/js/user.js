8ed24ad1e87212f7aa4eeff67c26397d6117c7b1
hyeonse <dlgustp1487@naver.com>
2020-12-10 21:08:14 +0900

    전통적인 로그인 및 시큐리티 적용위해 요청 주소 변경

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -4,6 +4,11 @@ let index = {
 												// 바인딩 하기 위해서!!!
 			   this.save();
 		   });
+		   $("#btn-login").on("click", () => {	// function(){} , () => {} this를
+												// 바인딩 하기 위해서!!!
+			   this.login();
+		   });
+
 		},
 
 		save: function() {
@@ -20,13 +25,41 @@ let index = {
 			// 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청!!
 			$.ajax({
 				type: "POST",
-				url: "/blog/api/user",
-				data: JSON.stringify(data), //http body 데이터
-				contentType: "application/json; charset=utf-8", // body데이터가 어떤타입인지(MIME)
-				dataType: "json" // 요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열 (생긴게 json이라면) => javascript오브젝트로 변경
+				url: "/api/user",
+				data: JSON.stringify(data), // http body 데이터
+				contentType: "application/json; charset=utf-8", // body데이터가
+																// 어떤타입인지(MIME)
+				dataType: "json" // 요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열 (생긴게
+									// json이라면) => javascript오브젝트로 변경
 			}).done(function(resp){
 				alert("회원가입이 완료되었습니다");
-				location.href = "/blog";
+				location.href = "/";
+			}).fail(function(error){
+				alert(JSON.stringify(error));
+			});
+		},
+		login: function() {
+			// alert("user의 save함수 호출됨");
+			let data = {
+					username : $("#username").val(),
+					password : $("#password").val(),
+			};
+
+			// console.log(data);
+
+			// ajax호출시 default가 비동기 호출
+			// 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청!!
+			$.ajax({
+				type: "POST",
+				url: "/api/user/login",
+				data: JSON.stringify(data), // http body 데이터
+				contentType: "application/json; charset=utf-8", // body데이터가
+																// 어떤타입인지(MIME)
+				dataType: "json" // 요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열 (생긴게
+									// json이라면) => javascript오브젝트로 변경
+			}).done(function(resp){
+				alert("로그인이 완료되었습니다");
+				location.href = "/";
 			}).fail(function(error){
 				alert(JSON.stringify(error));
 			});
