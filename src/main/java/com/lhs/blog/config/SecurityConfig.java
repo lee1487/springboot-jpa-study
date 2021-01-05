c5ba116bf2f1cf77109de3aafd39dd3f2fd38aa2
hyeonse <dlgustp1487@naver.com>
2021-01-05 21:35:45 +0900

    시큐리티 로그인 페이지 커스터마이징 && 비밀번호 해쉬후 로그인

---
+++ src/main/java/com/lhs/blog/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.lhs.blog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+//빈 등록: 스프링 컨테이너에서 객체를 관리할 수 있게 하는 것
+@Configuration	//빈등록 (IOC관리)
+@EnableWebSecurity	//시큐리티 필터 추가 = 스프링 시큐리티가 활성화가 되어 있는데 어떤 설정을 해당 파일에서 하겠다
+@EnableGlobalMethodSecurity(prePostEnabled = true) //특정 주소로 접근을 하면 권한 및 인증을 미리 체크하겠다는 뜻
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Bean //IOC가 됨
+	public BCryptPasswordEncoder encodePWD() {
+		return new BCryptPasswordEncoder();
+	}
+
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.csrf().disable()	//csrf 토큰 비활성화 (테스트시 걸어두는게 좋음)
+			.authorizeRequests()
+				.antMatchers("/","/auth/**","/js/**","/css/**","/image/**")
+				.permitAll()
+				.anyRequest()
+				.authenticated()
+			.and()
+			   	.formLogin()
+			   	.loginPage("/auth/loginForm");
+	}
+}
--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -4,10 +4,7 @@ let index = {
 												// 바인딩 하기 위해서!!!
 			   this.save();
 		   });
-		   $("#btn-login").on("click", () => {	// function(){} , () => {} this를
-												// 바인딩 하기 위해서!!!
-			   this.login();
-		   });
+
 
 		},
 
@@ -25,7 +22,7 @@ let index = {
 			// 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청!!
 			$.ajax({
 				type: "POST",
-				url: "/api/user",
+				url: "/auth/joinProc",
 				data: JSON.stringify(data), // http body 데이터
 				contentType: "application/json; charset=utf-8", // body데이터가
 																// 어떤타입인지(MIME)
@@ -33,32 +30,6 @@ let index = {
 									// json이라면) => javascript오브젝트로 변경
 			}).done(function(resp){
 				alert("회원가입이 완료되었습니다");
-				location.href = "/";
-			}).fail(function(error){
-				alert(JSON.stringify(error));
-			});
-		},
-		login: function() {
-			// alert("user의 save함수 호출됨");
-			let data = {
-					username : $("#username").val(),
-					password : $("#password").val(),
-			};
-
-			// console.log(data);
-
-			// ajax호출시 default가 비동기 호출
-			// 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청!!
-			$.ajax({
-				type: "POST",
-				url: "/api/user/login",
-				data: JSON.stringify(data), // http body 데이터
-				contentType: "application/json; charset=utf-8", // body데이터가
-																// 어떤타입인지(MIME)
-				dataType: "json" // 요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열 (생긴게
-									// json이라면) => javascript오브젝트로 변경
-			}).done(function(resp){
-				alert("로그인이 완료되었습니다");
 				location.href = "/";
 			}).fail(function(error){
 				alert(JSON.stringify(error));
