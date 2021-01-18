978269a5f03e84331acc5b35fbec3fa08d28d47f
hyeonse <dlgustp1487@naver.com>
2021-01-18 22:02:18 +0900

    글 목록보기, 글 목록 페이징, 글 상세보기 완료

--- src/main/java/com/lhs/blog/controller/BoardController.java
+++ src/main/java/com/lhs/blog/controller/BoardController.java
@@ -1,15 +1,34 @@
 package com.lhs.blog.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.lhs.blog.service.BoardService;
 
 @Controller
 public class BoardController {
 
+	@Autowired
+	private BoardService boardService;
+
 	@GetMapping({"","/"})
-	public String index() {
+	public String index(Model model, @PageableDefault(size=3, sort="id", direction=Sort.Direction.DESC) Pageable  pageable) {
+		model.addAttribute("boards", boardService.글목록(pageable));
 		// /WEB-INF/views/index.jsp
-		return "index";
+		return "index";	// viewResolver 작동!!
+	}
+
+	@GetMapping("/board/{id}")
+	public String findById(@PathVariable int id, Model model) {
+		model.addAttribute(boardService.글상세보기(id));
+
+		return "board/detail";
 	}
 
 	// USER 권한이 필요
--- src/main/java/com/lhs/blog/service/BoardService.java
+++ src/main/java/com/lhs/blog/service/BoardService.java
@@ -3,6 +3,8 @@ package com.lhs.blog.service;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,5 +23,16 @@ public class BoardService {
 		board.setCount(0);
 		board.setUser(user);
 		boardRepository.save(board);
+	}
+
+	public Page<Board> 글목록(Pageable pageable) {
+		return boardRepository.findAll(pageable);
+	}
+
+	public Board 글상세보기(int id) {
+		return boardRepository.findById(id)
+				.orElseThrow(() ->{
+					return new IllegalArgumentException("글 상세보기 실패: 아이디를 찾을 수 없습니다.");
+				});
 	}
 }
